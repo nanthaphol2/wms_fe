5943815e0097a0dd29fe06cc95b9917b8f344092
Nanthaphol <nanthaphol2@gmail.com>
2024-06-03 09:01:01 +0700

    Add material ui

--- wms_fe/src/app/app.component.ts
+++ wms_fe/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatFormFieldModule,MatInputModule,MatIconModule,MatButtonModule,MatCheckboxModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
